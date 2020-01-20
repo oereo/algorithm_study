724adeab5e36147d60cb79fbacd7c9ce04bbb289
oereo <dlstpgns0406@gmail.com>
2020-01-20 17:01:57 +0900

    N으로 표현

---
+++ STUDY(Thursday)/DP/N으로 표현.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int N = 0;
+int Number = 0;
+
+int solution(int N, int number) {
+    int answer = 0;
+    return answer;
+}
+
+void input(){
+
+    cin >> N >>Number;
+
+}
