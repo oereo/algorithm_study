ab7f23853d1b2b7ac402f686b33ad400e98110ee
oereo <dlstpgns0406@gmail.com>
2020-01-20 23:09:12 +0900

    10845_baekjun

--- STUDY(Thursday)/DP/N으로 표현.cpp
+++ STUDY(Thursday)/DP/N으로 표현.cpp
@@ -3,17 +3,37 @@
 #include <iostream>
 
 using namespace std;
+int result = 0;
+int cnt = 0;
+int b = 1;
+int ans = 0;
 
-int N = 0;
-int Number = 0;
-
-int solution(int N, int number) {
-    int answer = 0;
-    return answer;
+void DFS(int cnt,int length, int value, int target, vector<int> &numbers){
+    
+    if(length == cnt){
+        if(value == target){
+    
+            ans++;
+         }
+     return;   
+    }
+    
+    
+     DFS(cnt+1,length, value+numbers[cnt], target, numbers);
+     DFS(cnt+1,length, value-numbers[cnt], target, numbers);
+    
 }
 
-void input(){
-
-    cin >> N >>Number;
-
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    int length = numbers.size();
+    //int plus_cnt = numbers.size()-1;
+    //int minus_cnt = 1;
+        
+    DFS(1,length, numbers[0], target, numbers);
+    DFS(1,length, 0 - numbers[0], target, numbers);
+    
+    answer = ans;
+    
+    return answer;
 }
